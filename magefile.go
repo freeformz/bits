33a1a4bfe1d258797744a37005ea111670e2f0e1
Edward Muller <emuller@salesforce.com>
2019-11-01 16:28:15 -0700

    Dogfood

--- magefile.go
+++ magefile.go
@@ -4,11 +4,20 @@ package main
 
 import (
 	// mage:import
-	"github.com/freeformz/bits/mage/gons"
-	// mage:import
 	_ "github.com/freeformz/bits/mage/toolns"
+	// mage:import
+	"github.com/freeformz/bits/mage/gons"
 )
 
 func init() {
+	gons.ModuleName = "github.com/freeformz/bits"
 	gons.Version = "go1.13"
+
+	// Other gons settings (defaults)
+	// gons.CoverArgs  = "-html=coverage.out -o coverage.html"
+	// gons.TestArgs   = "-v -race -coverprofile=coverage.out -covermode=atomic ./..."
+
+	// Golangci-lint settings (defaults), remove the '_' part of the import above
+	// toolns.GolangciLint.Version = "1.20.0"
+	// toolns.GolangciLint.RunArgs = "--fix ./..."
 }
